5acc9d077d0c7433db86a123e572f9476291210f
Thanhche <thanhchenhat@yahoo.com>
2016-04-22 16:43:17 -0700

    Added files via upload

---
+++ tournament.sql
@@ -0,0 +1,25 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-- create table players(id_text text, name text, birthdate date);
+insert into players values('a1', 'Paul', '1997-06-06');
+insert into players values('a2', 'John', '1999-03-16');
+insert into players values('a3', 'Kenvin', '1999-02-26');
+insert into players values('a4', 'Kane', '2000-04-10');
+insert into players values('a5', 'Eric', '1999-07-12');
+insert into players values('a6', 'Jenifer', '1996-09-22');
+insert into players values('a7', 'Flora', '2002-05-17');
+insert into players values('a8', 'Jonathan', '2001-04-24');
+insert into players values('a9', 'Mark', '1998-02-25');
+insert into players values('a10', 'Manuel', '1998-03-14');
+insert into players values('a11', 'Sungjin', '1999-07-10');
+insert into players values('a12', 'Clair', '2000-08-19');
+insert into players values('a13', 'Henry', '1998-05-04');
+insert into players values('a14', 'Lucia', '1997-12-11');
+insert into players values('a15', 'Maria', '1997-11-24');
+insert into players values('a16', 'Diana', '1999-10-14');
